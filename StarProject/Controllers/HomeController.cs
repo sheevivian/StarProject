956ad8bc9ab6a5749bc8498d4f025816a2294d01
wei <sheevivian@gmail.com>
2025-09-02 19:49:54 +0800

    Error頁面修改

--- StarProject/Controllers/HomeController.cs
+++ StarProject/Controllers/HomeController.cs
@@ -20,8 +20,8 @@ namespace StarProject.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
-        }
+			return View();
+		}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()

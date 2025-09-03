8b08bc70453cbcfd5ff91d7a983adb1f655770ac
yifi <chc72281xyz@gmail.com>
2025-09-03 15:26:35 +0800

    新增所需資料夾、新增活動列表頁的EventController.cs - index, details and some new codes.

--- StarProject/Models/Event.cs
+++ StarProject/Models/Event.cs
@@ -31,7 +31,7 @@ public partial class Event
 
     public int? Deposit { get; set; }
 
-    public string Image { get; set; } = null!;
+    public string? Image { get; set; }
 
     public virtual ICollection<Participant> Participants { get; set; } = new List<Participant>();
 }
